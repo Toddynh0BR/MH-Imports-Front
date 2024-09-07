2aa8f40186ce157d73ebcef1dba022000bab3f2c
Matheus <galaxyplay41@gmail.com>
2024-09-07 18:53:09 -0300

    Adição de analityc

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { register } from "swiper/element-bundle";
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import GlobalStyle from './style/global';
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
+import { Routes } from './routes';
 
 register();
 import "swiper/css/navigation";
@@ -9,11 +10,32 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css";
 
-import { Routes } from './routes';
+function initializeAnalytics() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ window.dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'G-WPZEL8451B');
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <GlobalStyle/>
+function App() {
+  useEffect(() => {
+
+    const script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-WPZEL8451B";
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      initializeAnalytics();
+    };
+  }, []); 
+
+  return (
+    <StrictMode>
+      <GlobalStyle />
       <Routes />
-  </StrictMode>,
-)
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')).render(<App />);
